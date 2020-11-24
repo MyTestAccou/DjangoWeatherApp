ce08c687d8c6d02df17318f03e89b8caf6cf53c8
Dima <haveadrinkonme666@gmail.com>
2020-11-24 13:00:31 +0300

    'work app'

--- app/the_weather/weather/views.py
+++ app/the_weather/weather/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 
 # Create your views here.
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=ru&appid=947a289fadf70c93946c0d463998271a'
-    city = 'Las Vegas'
-    city_weather = requests.get(url.format(city)).json() # Запрос к АПИ и конвертация в  JSON
+    appid = '947a289fadf70c93946c0d463998271a'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=ru&appid=' + appid
+    cities = City.objects.all()
 
-    weather = {
-        'city': city,
-        'temperature': city_weather['main']['temp'],
-        'description': city_weather['weather'][0]['description'],
-        'icon': city_weather['weather'][0]['icon']
-    }
-    context = {'weather': weather}
-    return render(request, 'weather/index.html')
+    if request.method == 'POST': # only true if form use
+        form = CityForm(request.POST) # add actual request data from poccessing
+        form.save() # save what we add
+
+    form = CityForm()
+
+    weather_data = []
+
+    for city in cities:  #return all cities in database
+        city_weather = requests.get(url.format(city)).json() # Запрос к АПИ и конвертация в  JSON
+    # print(city_weather.text)
+
+
+        weather = {
+            'city': city.name,
+            'temperature': city_weather["main"]["temp"],
+            'description': city_weather["weather"][0]["description"],
+            'icon': city_weather["weather"][0]["icon"]
+        }
+        weather_data.append(weather) # add data for current city into our list
+
+    context = {'weather_data': weather_data, 'form': form}
+    return render(request, 'weather/index.html', context)
